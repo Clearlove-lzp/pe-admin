67faa4b26045d48f5b4efaa76d9cfc89a7407cfa
Clearlove-lzp <1587029518@qq.com>
2021-04-01 10:47:59 +0800

    add new

---
+++ src/views/common/home.vue
@@ -0,0 +1,23 @@
+<!-- 首页 -->
+<template>
+  <div>首页</div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data () {
+    return {
+    };
+  },
+  components: {},
+  computed: {},
+  methods: {},
+  watch: {},
+  mounted() {},
+  created() {},
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/common/register.vue
@@ -0,0 +1,23 @@
+<!-- 注册页面 -->
+<template>
+  <div>注册页面</div>
+</template>
+
+<script>
+export default {
+  props: {},
+  data () {
+    return {
+    };
+  },
+  components: {},
+  computed: {},
+  methods: {},
+  watch: {},
+  mounted() {},
+  created() {},
+}
+</script>
+
+<style scoped>
+</style>
